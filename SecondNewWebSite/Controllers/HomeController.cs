e315e8455ef2cd7d5fe82bbcb2c3791116e3b3d5
Dyyenice <dyyenice@gmail.com>
2023-11-02 19:47:43 +0300

    Second hand online store

---
+++ SecondNewWebSite/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecondNewWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewProduct()
+        {
+            
+            return View();
+        }
+
+        public ActionResult editmyprofile()
+        {
+            
+            return View();
+        }
+
+        public ActionResult EditProduct()
+        {
+
+            return View();
+        }
+        public ActionResult favorites()
+        {
+
+            return View();
+        }
+        public ActionResult filter()
+        {
+
+            return View();
+        }
+        public ActionResult myhomepage()
+        {
+
+            return View();
+        }
+
+        public ActionResult myprofile()
+        {
+
+            return View();
+        }
+        public ActionResult navbar()
+        {
+
+            return View();
+        }
+        public ActionResult otheruserprofile()
+        {
+
+            return View();
+        }
+        public ActionResult signin()
+        {
+
+            return View();
+        }
+        public ActionResult signup()
+        {
+
+            return View();
+        }
+        public ActionResult ViewProduct()
+        {
+
+            return View();
+        }
+    }
+}
